85a0f06d33e04bba3c2817232e6b1f9cf7b4d7bb
Artush-Baghdasaryan <artush.baghdasaryan@mail.ru>
2024-11-02 03:29:26 +0300

    redirect to core backend

--- authorizations/src/settings.py
+++ authorizations/src/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class BaseHackathonSettings(BaseSettings):
-    user_token_from_tg_bot: str = ""
+    user_token_from_tg_bot: str = "7884940461:AAHeRLMuv8Kbn2eXfdGusxhd-CwPeNrXckg"
     save_auth_data_endpoint: str = "https://aes-agniachallenge-case.olymp.innopolis.university/save-authorization-data"
 
 
@@ -10,11 +10,17 @@ class TodoistAuthSettings(BaseSettings):
     todoist_oauth_api_url: str = "https://todoist.com/oauth/authorize/"
     todoist_token_exchange_api_url: str = "https://todoist.com/oauth/access_token/"
     todoist_redirect_url: str = "http://localhost:9000"
-    todoist_client_id: str = ""
+    todoist_client_id: str = "c2778732236a4499bf8d3aa9d18f78a2"
     todoist_scope: str = "task:add,data:read,data:read_write,data:delete"
     todoist_state: str = "some_secret_state"
-    todoist_client_secret: str = ""
+    todoist_client_secret: str = "d7be3872056e4bbab047abf9f316e583"
+
+class CoreServiceSettings(BaseSettings):
+    base_api_url: str = "http://localhost:5004"
+    todoist_authorize_url: str = "todoist/authorize"
+    todoist_get_token_url: str = "todoist/get-token"
 
 
 base_hackathon_settings = BaseHackathonSettings()
 todoist_auth_settings = TodoistAuthSettings()
+core_service_settings = CoreServiceSettings()
